9e79fc39b1cd820d5cee6bc073a583230894c1df
Momoka Kohara <koharamomoka@gmail.com>
2023-07-28 01:31:28 +0900

    angular/fire

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'penguin-room',
+    appId: '1:364951968246:web:b2e54d9100b25e74f48fd7',
+    storageBucket: 'penguin-room.appspot.com',
+    apiKey: 'AIzaSyBvV1vbGMBUq84p9Wg1tAk3ObW2-FDkMw0',
+    authDomain: 'penguin-room.firebaseapp.com',
+    messagingSenderId: '364951968246',
+  },
+};
