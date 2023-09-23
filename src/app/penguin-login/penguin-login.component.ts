5ad7822f0531e8abc2323a80d6ad6c7b785266af
mkatsys <kohara@atsys.co.jp>
2023-09-24 00:46:38 +0900

    ログイン機能（addData）

--- src/app/penguin-login/penguin-login.component.ts
+++ src/app/penguin-login/penguin-login.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { inject } from '@angular/core';
-import { Firestore, collectionData, collection } from '@angular/fire/firestore';
+import { Firestore, collectionData, collection, addDoc } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 
 interface Item {
@@ -17,12 +17,25 @@ export class PenguinLoginComponent {
   // item$: Observable<Item[]>;
   // firestore: Firestore = inject(Firestore);
 
-  constructor() {
+  constructor(private firestore : Firestore) {
     // const itemCollection = collection(this.firestore, 'items');
     // this.item$ = collectionData(itemCollection);
   }
 
   addData(f:any){
     console.log(f.value);
+    //コレクション'users'接続
+    const collectionInstance = collection(this.firestore, 'users');
+    //データ追加
+    addDoc(collectionInstance, f.value).then(() =>{
+      console.log('Data Saved Successfully');
+    }).catch((err) =>{
+      //Missing or insufficient permissions. の場合、firestore databaseのtimestampの日付を書き換える
+      console.log(err);
+    })
+  }
+
+  getData(){
+    
   }
 }
--- src/app/services/penguin-auth.service.ts
+++ src/app/services/penguin-auth.service.ts
@@ -1,7 +1,6 @@
 import { Injectable, Optional } from '@angular/core';
-import { signInWithEmailAndPassword, signOut } from 'firebase/auth';
-import { Auth, getAuth, signInAnonymously } from '@angular/fire/auth';
-import { authState } from 'rxfire/auth';
+import { inject } from '@angular/core';
+import { Firestore, collectionData, collection, addDoc, doc, deleteDoc } from '@angular/fire/firestore';
 
 
 @Injectable({
@@ -9,15 +8,32 @@ import { authState } from 'rxfire/auth';
 })
 export class PenguinAuthService {
 
-  constructor(@Optional() public fireAuth: Auth) {
+  constructor(private fs : Firestore) {
     
-   }
+  }
+
+  getNote(){
+    let notesCollection = collection(this.fs, 'notes');
+    return collectionData(notesCollection,{idField:'id'});
+  }
+
+  addNote(desc:string){
+    let data = {description:desc};
+    let notesCollection = collection(this.fs, 'notes');
+    return addDoc(notesCollection, data).then(() =>{
+      console.log('Data Saved Successfully');
+    }).catch((err) =>{
+      console.log(err);
+    })
+  }
+
+  deleteNote(id:string){
+    let docRef = doc(this.fs,'notes/'+id);
+    return deleteDoc(docRef);
+  }
 
   login(){
-    const auth = getAuth();
-    signInAnonymously(auth).then(() => {
-    // Signed in..
-  })
+
   }
 
   logout(){
